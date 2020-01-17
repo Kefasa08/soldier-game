128bce41f686f1e466b877e240a4c63378afedfe
DavidWelander <56589906+DavidWelander@users.noreply.github.com>
2020-01-17 09:40:31 +0100

    Fixad version av spelet

--- Assets/Scripts+sprites/contorls.cs
+++ Assets/Scripts+sprites/contorls.cs
@@ -6,13 +6,15 @@ public class contorls : MonoBehaviour
 {
     Rigidbody2D RB;
     [SerializeField]
-    private bool IsGrounded;
-    [SerializeField]
     Sprite Shouvle_Out;
     Sprite original_sprite;
     Animator Anim;
+    public bool IsGrounded;
+    public bool inAirCollision;
+    public bool DigAnim = false;
+
     // Start is called before the first frame update
-    public void Start()
+    void Start()
     {
         RB = GetComponent<Rigidbody2D>();
         Anim = GetComponent<Animator>();
@@ -20,82 +22,94 @@ public class contorls : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void Update()
+    void Update()
     {
-        
-        if (Input.GetKey(KeyCode.D))
+
+        if (Input.GetKey(KeyCode.D) && !inAirCollision)
         {
-            RB.velocity = new Vector3(5, RB.velocity.y, 0);
+            RB.velocity = new Vector3(2, RB.velocity.y, 0);
             Anim.SetBool("Walking", true);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && !inAirCollision)
         {
+            Debug.Log(":)");
 
             Anim.SetBool("Walking", true);
-            RB.velocity = new Vector3(-5, RB.velocity.y, 0);
+            RB.velocity = new Vector3(-2, RB.velocity.y, 0);
+            //RB.velocity = Vector3.zero;
+
         }
 
         if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
         {
             Anim.SetBool("Walking", false);
             RB.velocity = new Vector3(0, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.W) && IsGrounded==true) 
-        {
-            RB.velocity = new Vector3(RB.velocity.x, 7, 0);
-            IsGrounded = false;
-        }
-        
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && IsGrounded == true)
+        {
+            RB.velocity = new Vector3(RB.velocity.x, 5, 0);
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             // SR.sprite = Shouvle_Out;
             if (Anim)
             {
                 Debug.Log("ja");
-               
+
+
             }
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
-            
+
         }
 
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (IsGrounded == false)
+        {
+            Debug.Log("hej");
+            inAirCollision = true;
+            RB.velocity = new Vector3(0, RB.velocity.y, 0);
+        }
 
     }
     void OnTriggerStay2D(Collider2D Trigger)
     {
-        if (Anim)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
+
             if (Trigger.tag == "dig")
             {
                 Destroy(Trigger.gameObject);
+                Anim.SetBool("Walking", false);
+                Anim.SetTrigger("Digging");
+
+            }
+            else if (Trigger.tag != "dig")
+            {
+
             }
         }
-        if (Trigger.tag == "soldier" && Input.GetKeyDown(KeyCode.E))
-        {
-            Destroy(Trigger.gameObject);
-            Anim.SetBool("Walking", true);
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        IsGrounded = true;
+
+
     }
 
-     
-        
+
+
+
     private void OnTrigger(Collider2D Trigger)
     {
-       
-    
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-       
-    }
 
 
+    }
 
 }
