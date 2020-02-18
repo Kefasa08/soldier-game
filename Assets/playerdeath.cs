5ce2942c76059fcd968144e7393ffe2f7fbf3bc6
Kygar0 <56590264+Kygar0@users.noreply.github.com>
2020-02-18 11:07:55 +0100

    fixat death screen

---
+++ Assets/playerdeath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerdeath : MonoBehaviour
+{
+    [SerializeField]
+    GameObject player;
+
+    void Update()
+    {
+        if (player == null)
+        {
+            print("aa");
+            SceneManager.LoadScene(2);
+
+        }
+    }
+}
