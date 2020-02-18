96a4277ff61732cede1c45796e8cfc3257eabf84
DavidWelander <56589906+DavidWelander@users.noreply.github.com>
2020-02-18 10:11:07 +0100

    Backgroundsflip

---
+++ Assets/Scripts+sprites/Backgroundflip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroundflip : MonoBehaviour
+{
+    public bool facingRight = true;
+    public float moveInput;
+    public void FixedUpdate()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
