4df91871ce6b33fbbdcbbc414362661e4b66ad7d
DavidWelander <56589906+DavidWelander@users.noreply.github.com>
2020-02-29 13:12:18 +0100

    (Fixed) shooting

--- Assets/Scripts+sprites/Bullet.cs
+++ Assets/Scripts+sprites/Bullet.cs
@@ -6,23 +6,23 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField]
     float damage = 5f;
-    Rigidbody bullet;
+    Rigidbody2D bullet;
 
     public float direction;
     void Start()
     {
-        bullet = GetComponent<Rigidbody>();
+        bullet = GetComponent<Rigidbody2D>();
         //bullet.velocity = new Vector3(100, 0, 0);
         print(transform.position);
     }
 
     public void Update()
     {
-        Invoke("Kill", 0.7f);
-        bullet.velocity = new Vector3(100 * direction, 0, 0);
+       // Invoke("Kill", 0.7f);
+        //bullet.velocity = new Vector3(100 * direction, 0, 0);
 
     }
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         collision.gameObject.GetComponent<healthscript>().TakeDamage(damage);
         Destroy(gameObject);
---
+++ Assets/Scripts+sprites/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    public float damage = 5f;
+    public float lifeTime;
+    void Start()
+    {
+        Invoke("DestroyProjectile", lifeTime);
+    }
+    void Update()
+    {
+        transform.Translate(transform.up * speed * Time.deltaTime);
+    }
+    void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.GetComponent<healthscript>().TakeDamage(damage);
+        Destroy(gameObject);
+    }
+}
