ce763c44c3574d3ca409dbc75efc9d9bfcedbdd5
DavidWelander <56589906+DavidWelander@users.noreply.github.com>
2020-03-17 11:18:13 +0100

    Screenshake

---
+++ Assets/Scripts+sprites/ScreenShakeController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShakeController : MonoBehaviour
+{
+    private float shakeTimeRemaining, shakePower;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            StartShake(.5f, 1f);
+        }
+    }
+    private void LateUpdate()
+    {
+        if (shakeTimeRemaining > 0)
+        {
+            shakeTimeRemaining -= Time.deltaTime;
+
+            float xAmount = Random.Range(-1f, 1f) * shakePower;
+            float yAmount = Random.Range(-1f, 1f) * shakePower;
+
+            transform.position += new Vector3(xAmount, yAmount, 0f);
+        }
+    }
+    public void StartShake(float length, float power)
+    {
+        shakeTimeRemaining = length;
+        shakePower = power;
+    }
+}
